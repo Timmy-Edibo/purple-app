54b1f8c31683ef54447bd077b7f3d3dd89e9a4bc
timmy <timothyedibo@gmail.com>
2024-05-04 22:08:01 +0100

    pushing to github

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Learning DevOps with Uncle Mike</h1>
+      <h3>List of thing to do: </h3>
+
+      <li>Write our react application</li>
+      <li>Push to github</li>
+      <li>Pull code to ubuntu server</li>
+      <li>Deploy react application</li>
+
+    </div>
+  );
+}
+
+export default App;
