a53e1417bf6105100ac995ba210c95288a465469
timmy <timothyedibo@gmail.com>
2024-05-11 00:53:50 +0100

    workflow with jenkins

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ls -lrt
+node --version && npm --version
+npm run build
+ls /var/www/react_app || true
+sudo rm -rf /var/www/react_app ||true
+ls -l /var/www/
+sudo cp -r build /var/www/
+sudo mv /var/www/build /var/www/react_app
+sudo systemctl restart nginx && sudo systemctl daemon-reload
