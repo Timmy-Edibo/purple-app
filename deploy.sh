5020a573c8981f1306389688b0d4d7ab3819ff97
timmy <timothyedibo@gmail.com>
2024-05-11 01:34:25 +0100

    workflow with jenkins

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 ls -lrt
 ls /var/www/react_app || true
-sudo rm -rf /var/www/react_app ||true
+rm -rf /var/www/react_app ||true
 ls -l /var/www/
-sudo cp -r build /var/www/
-sudo mv /var/www/build /var/www/react_app
-sudo systemctl restart nginx && sudo systemctl daemon-reload
+cp -r build /var/www/
+mv /var/www/build /var/www/react_app
+systemctl restart nginx && sudo systemctl daemon-reload
